efe06a15a3093aa351ed4185045e8d6a674eeee5
Tonmoy Chiran <Tonmoy Chiran>
2020-06-03 14:40:08 +0600

    Db config changed

--- Database/db.js
+++ Database/db.js
@@ -7,8 +7,8 @@ let config = require('../config');
     dbURL = `mongodb+srv://uddogtahat:uddogtahat54321@cluster0-gsey2.mongodb.net/uddogtahat?retryWrites=true&w=majority`;
 }*/
 //let dbURL = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`; //development mode url
-//dbURL = `mongodb+srv://uddogtahat:uddogtahat54321@cluster0-gsey2.mongodb.net/uddogtahat?retryWrites=true&w=majority`;
-let dbURL = `mongodb+srv://${process.env.DB_HOST}:${process.env.DB_PASS}@cluster0-gsey2.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+let dbURL = `mongodb+srv://uddogtahat:uddogtahat54321@cluster0-gsey2.mongodb.net/uddogtahat?retryWrites=true&w=majority`;
+//let dbURL = `mongodb+srv://${process.env.DB_HOST}:${process.env.DB_PASS}@cluster0-gsey2.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 
 module.exports=dbURL;
